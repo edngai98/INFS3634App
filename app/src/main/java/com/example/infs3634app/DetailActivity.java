30554d016d162e093bf179ddf218d1c7d62b73da
Edmond Ngai <ed_ngai98@hotmail.com>
2020-03-24 18:56:32 +1100

    Draft 2

--- app/src/main/java/com/example/infs3634app/DetailActivity.java
+++ app/src/main/java/com/example/infs3634app/DetailActivity.java
@@ -10,5 +10,15 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        if (savedInstanceState == null) {
+            Bundle arguments = new Bundle();
+            arguments.putString(DetailFragment.ARG_ITEM_ID, getIntent().getStringExtra(DetailFragment.ARG_ITEM_ID));
+            DetailFragment fragment = new DetailFragment();
+            fragment.setArguments(arguments);
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.detail_container, fragment)
+                    .commit();
+        }
     }
 }
